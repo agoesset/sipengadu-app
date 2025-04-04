3d843d4b3487c4764f2d8837f817239f2426f282
agoesset <agussetiawan160802@gmail.com>
2025-04-04 23:24:06 +0700

    Update tahun copyright

--- resources/views/includes/front/footer.blade.php
+++ resources/views/includes/front/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="footer clearfix mb-0 text-muted">
             <div class="float-start">
-                <p>2024 &copy; Sipengadu</p>
+                <p>2025 &copy; Sipengadu</p>
             </div>
             <div class="float-end">
                 <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
